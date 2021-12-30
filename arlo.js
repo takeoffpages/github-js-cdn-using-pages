3db59e8d5b070e8be068f37e211ff81598729e81
takeoffpages <95906491+takeoffpages@users.noreply.github.com>
2021-12-31 01:34:07 +0600

    Update arlo.js

--- arlo.js
+++ arlo.js
@@ -2,7 +2,7 @@ document.addEventListener("arlojscontrolsloaded", function () {
     var upcomingEventsControl = {
         moduleType: "UpcomingEvents",
         targetElement: "#upcoming-events-control1",                        
-        template: "#upcoming-events-control1-template",
+        template: "#1-test-event-1",
         customUrls: {
             eventtemplate: "https://arlo-integration.webflow.io/event-template"
         }
