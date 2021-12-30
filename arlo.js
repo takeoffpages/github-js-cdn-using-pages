e6b06ad85a74c2112d565986ef9f3c9568a46fb1
takeoffpages <95906491+takeoffpages@users.noreply.github.com>
2021-12-30 14:35:45 +0600

    Update arlo.js

--- arlo.js
+++ arlo.js
@@ -1,1 +1,32 @@
-alert('hey');
+<script>
+    document.addEventListener("arlojscontrolsloaded", function () {
+        var upcomingEventsControl = {
+            moduleType: "UpcomingEvents",
+            targetElement: "#upcoming-events-control1",                        
+            template: "#upcoming-events-control1-template",
+            customUrls: {
+                eventtemplate: "http://example.com/eventtemplatepage.html",
+                venue: "http://example.com/venue.html",
+                presenter: "http://example.com/presenter.html"
+            },
+            top: 5
+        };
+        var upcomingEventsControl2 = {
+            moduleType: "UpcomingEvents",
+            targetElement: "#upcoming-events-control2",                        
+            template: "#upcoming-events-control2-template",
+            customUrls: {
+                eventtemplate: "http://example.com/eventtemplatepage.html",
+                venue: "http://example.com/venue.html",
+                presenter: "http://example.com/presenter.html"
+            },
+            top: 10
+        };
+        new ArloWebControls().start({
+           "platformID": "demo.arlo.co",
+            "modules": [
+                upcomingEventsControl, 
+                upcomingEventsControl2
+        ]});
+     });
+</script>
